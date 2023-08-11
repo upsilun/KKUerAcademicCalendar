79df56133dcf4f9913887fee5525a923530aa994
Yazeed Alahmad <72664813+upsilun@users.noreply.github.com>
2023-08-11 19:19:18 +0300

    "src" files added successfully ✔

---
+++ src/index.js
@@ -0,0 +1,389 @@
+var arrayOfObjects = 
+[
+    {
+        title: "بداية الفصل الصيفي",
+        date: "2023/07/02",
+        dateHijri: "1444/12/16",
+        day: "غير مسجل"
+    }, 
+
+
+    {
+        title: "عودة اعضاء هيئة التدريس وبداية تأجيل الدراسة للفصل الاول",
+        date: "2023/08/13",
+        dateHijri: "1445/01/26",
+        day: "الأحد"
+    }, 
+
+    // Summer Semester 
+    {
+        title: "نهاية الفصل الصيفي",
+        date: "2023/08/15",
+        dateHijri: "1444/1/28",
+        day: "الثلاثاء"
+    },
+    // End Summer Semester 
+
+    {
+        title: "اخر موعد لتأجيل الدراسة",
+        date: "2023/08/19",
+        dateHijri: "1445/02/03",
+        day: "السبت"
+    }, 
+
+    // 1st Semester
+
+    {
+        title: "s1",
+        date: "2023/08/20",
+        dateHijri: "1445/02/04",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "بداية الدراسة",
+        date: "2023/08/20",
+        dateHijri: "1445/02/04",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اجازة اليوم الوطني",
+        date: "2023/09/24",
+        dateHijri: "1445/03/09",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اجازة نهاية اسبوع مطولة",
+        date: "2023/11/02",
+        dateHijri: "1445/04/18",
+        day: "الخميس"
+    }, 
+
+    {
+        title: "بداية اجازة منتصف الفصل",
+        date: "2023/11/16",
+        dateHijri: "1445/05/02",
+        day: "الخميس"
+    }, 
+
+    {
+        title: "استئناف الدراسة بعد اجازة منتصف الفصل",
+        date: "2023/11/26",
+        dateHijri: "1445/05/12",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اخر موعد للاعتذار عن دراسة فصل او مقرر",
+        date: "2023/11/26",
+        dateHijri: "1445/05/12",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اخر موعد للانسحاب من الجامعة بدون رسوب",
+        date: "2023/12/11",
+        dateHijri: "1445/05/26",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اجازة نهاية اسبوع مطولة",
+        date: "2023/12/17",
+        dateHijri: "1445/06/04",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "بداية الاختبارات النهائية",
+        date: "2023/12/18",
+        dateHijri: "1445/06/05",
+        day: "الاثنين"
+    }, 
+
+    {
+        title: "نهاية الفصل الدراسي الاول",
+        date: "2024/01/04",
+        dateHijri: "1445/06/22",
+        day: "الاثنين"
+    }, 
+
+    // 1st Semester end
+
+    {
+        title: "s2",
+        date: "2024/01/07",
+        dateHijri: "1445/06/25",
+        day: "الأحد"
+    },
+
+    // 2nd Semester
+
+    {
+        title: "بداية تأجيل الدراسة",
+        date: "2024/01/07",
+        dateHijri: "1445/06/25",
+        day: "الأحد"
+    },
+
+    {
+        title: "اخر موعد لتأجيل الدراسة",
+        date: "2024/01/13",
+        dateHijri: "1445/07/01",
+        day: "السبت"
+    }, 
+    
+    {
+        title: "بداية الدراسة",
+        date: "2024/01/14",
+        dateHijri: "1445/07/02",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اجازة نهاية اسبوع مطولة",
+        date: "2024/01/28",
+        dateHijri: "1445/07/16",
+        day: "الأحد"
+    }, 
+
+    {
+        title: "اجازة يوم التأسيس وبداية اجازة منتصف الفصل",
+        date: "2024/02/22",
+        dateHijri: "1445/08/12",
+        day: "الخميس"
+    }, 
+    
+    {
+        title: "استئناف الدراسة بعد اجازة منتصف الفصل",
+        date: "2024/03/03",
+        dateHijri: "1445/08/22",
+        day: "الأحد"
+    }, 
+        
+    {
+        title: "بداية اجازة عيد الفطر",
+        date: "2024/03/28",
+        dateHijri: "1445/09/18",
+        day: "الخميس"
+    }, 
+
+    {
+        title: "بداية الدراسة بعد اجازة عيد الفطر",
+        date: "2024/04/15",
+        dateHijri: "1445/10/06",
+        day: "الاثنين"
+    }, 
+    
+    {
+        title: "اجازة نهاية اسبوع مطولة",
+        date: "2024/05/02",
+        dateHijri: "1445/10/23",
+        day: "الخميس"
+    }, 
+
+    {
+        title: "اجازة نهاية اسبوع مطولة",
+        date: "2024/05/02",
+        dateHijri: "1445/10/23",
+        day: "الخميس"
+    }, 
+
+    {
+        title: "اخر موعد للاعتذار عن دراسة فصل أو مقرر",
+        date: "2024/05/06",
+        dateHijri: "1445/10/27",
+        day: "الاثنين"
+    }, 
+    
+    {
+        title: "اخر موعد للانسحاب من الجامعة بدون رسوب",
+        date: "2024/05/12",
+        dateHijri: "1445/11/04",
+        day: "الأحد"
+    }, 
+        
+    {
+        title: "بداية الاختبارات النهائية",
+        date: "2024/05/19",
+        dateHijri: "1445/11/11",
+        day: "الأحد"
+    }, 
+            
+    {
+        title: "نهاية الفصل الدراسي الثاني",
+        date: "2024/05/19",
+        dateHijri: "1445/11/11",
+        day: "الأحد"
+    }, 
+
+
+    // 2nd Semester Ends
+
+    {
+        title: "s3",
+        date: "2024/06/24",
+        dateHijri: "1445/12/17",
+        day: "الأحد"
+    }, 
+
+    // Summer Semester
+            
+    {
+        title: "عودة اعضاء هيئة التدريس وبداية الدراسة",
+        date: "2024/06/24",
+        dateHijri: "1445/12/17",
+        day: "الأحد"
+    }, 
+                
+    {
+        title: "نهاية الفصل الصيقي",
+        date: "2024/08/15",
+        dateHijri: "1445/12/17",
+        day: "الأحد"
+    }, 
+
+
+];
+
+
+
+function cl(text) {
+    console.log(text)
+}
+
+
+// Function to calculate the remaining days from a given date
+function calculateRemainingDays(dateInput) {
+    var countDownDate = new Date(dateInput).getTime();
+    var currentTime = new Date().getTime();
+    var timeDifference = countDownDate - currentTime;
+    var remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
+
+    if (timeDifference < 0) {
+        return "0";
+    } else {
+        return remainingDays;
+    }
+}
+
+// Function to calculate the remaining hours from a given date
+function calculateRemainingHours(dateInput) {
+    var countDownDate = new Date(dateInput).getTime();
+    var currentTime = new Date().getTime();
+    var timeDifference = countDownDate - currentTime;
+    var remainingHours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    if (timeDifference < 0) {
+        return "0";
+    } else {
+        return remainingHours;
+    }
+}
+
+// Function to check if a given date is today's date
+function isDateToday(dateString) {
+    const inputDate = new Date(dateString);
+    const today = new Date();
+
+    return (
+        inputDate.getDate() === today.getDate() &&
+        inputDate.getMonth() === today.getMonth() &&
+        inputDate.getFullYear() === today.getFullYear()
+    );
+}
+
+// Variables to keep track of the current event index and other settings
+var isFirstValidEvent = false;
+var currentIndex = 0;
+
+// Loop through the array of objects
+for (var i = currentIndex; i < arrayOfObjects.length; i++) {
+    // Log separator for each iteration
+    cl("\n\n")
+
+    // Update the current index and get the current object
+    currentIndex = i;
+    var currentObject = arrayOfObjects[i];
+    var remainingDaysOfObject = calculateRemainingDays(arrayOfObjects[i].date);
+    var remainingHoursOfObject = calculateRemainingHours(arrayOfObjects[i].date);
+
+    // Check if the current object title is one of "s1", "s2", or "s3"
+    if (arrayOfObjects[i].title == "s1" || arrayOfObjects[i].title == "s2" || arrayOfObjects[i].title == "s3") {
+        // Append appropriate elements based on the title
+        $('body').append('<div class="line_end"></div>');
+        if (arrayOfObjects[i].title == "s1") {
+            $('body').append('<h4 class="semester_name">الفصل الاول</h4>');
+        } else if (arrayOfObjects[i].title == "s2") {
+            $('body').append('<h4 class="semester_name">الفصل الثاني</h4>');
+        } else if (arrayOfObjects[i].title == "s3") {
+            $('body').append('<h4 class="semester_name">الفصل الصيفي</h4>');
+        }
+
+        // Skip to the next iteration
+        continue;
+    }
+
+    // Log the current index
+    cl(currentIndex)
+
+    // If the event date is today, set remainingHoursOfObject to -1
+    if (isDateToday(arrayOfObjects[i].date)) {
+        remainingHoursOfObject = -1;
+    }
+
+    // Check if there are remaining days or hours for the event
+    if (remainingDaysOfObject != 0 || remainingHoursOfObject != 0) {
+        if (isFirstValidEvent == false) {
+            // Handle the first valid event differently
+
+            // Set the flag for the first valid event
+            isFirstValidEvent = Boolean(true);
+
+            // Append elements for the first valid event
+            $('body').append('<p class="p_id">' + currentIndex + '</p> <div class="event event_over shadow"><h1>' + arrayOfObjects[i - 1].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + 'منتهي' + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i - 1].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i - 1].dateHijri + '</h5><h6>' + arrayOfObjects[i - 1].day + '</h6></div>');
+
+            // Check if the event is today or within 24 hours
+            if (remainingHoursOfObject == -1) {
+                // Event is today
+                $('body').append('<p class="p_id">' + currentIndex + '<div class="event event_near shadow"><h1>' + arrayOfObjects[i].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + 'اليوم' + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i].dateHijri + '</h5><h6>' + arrayOfObjects[i].day + '</h6></div>');
+                // Skip to the next iteration
+                continue;
+            }
+
+            // Check if the event is within 24 hours and the event is not today
+            if (remainingHoursOfObject <= 24 && remainingDaysOfObject == 0) {
+                // Event is within 24 hours
+                $('body').append('<p class="p_id">' + currentIndex + '<div class="event event_near shadow"><h1>' + arrayOfObjects[i].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">ايام</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + 'يوم' + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i].dateHijri + '</h5><h6>' + arrayOfObjects[i].day + '</h6></div>');
+                // Skip to the next iteration
+                continue;
+            }
+
+            // Check if the event has 10 or fewer remaining days
+            if (remainingDaysOfObject <= 10) {
+                // Event has 10 or fewer remaining days
+                $('body').append('<p class="p_id">' + currentIndex + '<div class="event event_near shadow"><h1>' + arrayOfObjects[i].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">ايام</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + remainingDaysOfObject + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i].dateHijri + '</h5><h6>' + arrayOfObjects[i].day + '</h6></div>');
+            } else {
+                // Event has more than 10 remaining days
+                $('body').append('<p class="p_id">' + currentIndex + '<div class="event event_near shadow"><h1>' + arrayOfObjects[i].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">يوم</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + remainingDaysOfObject + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i].dateHijri + '</h5><h6>' + arrayOfObjects[i].day + '</h6></div>');
+            }
+
+        } else {
+            // Handle events after the first valid event
+
+            // Check if the event has 10 or fewer remaining days
+            if (remainingDaysOfObject <= 10) {
+                // Event has 10 or fewer remaining days
+                $('body').append('<p class="p_id">' + currentIndex + '<div class="event shadow"><h1>' + arrayOfObjects[i].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">ايام</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + remainingDaysOfObject + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i].dateHijri + '</h5><h6>' + arrayOfObjects[i].day + '</h6></div>')
+            } else {
+                // Event has more than 10 remaining days
+                $('body').append('<p class="p_id">' + currentIndex + '<div class="event shadow"><h1>' + arrayOfObjects[i].title + '</h1><h4 style="color: gray;">متبقي</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">يوم</h4><h2 style="display: inline-block; padding-inline: 3px; text-decoration: underline;">' + remainingDaysOfObject + '</h2><h5 style="margin-top: 20px;">' + arrayOfObjects[i].date + ' <span class="dates_dot">-</span> ' + arrayOfObjects[i].dateHijri + '</h5><h6>' + arrayOfObjects[i].day + '</h6></div>')
+            }
+        }
+    } else {
+        // If there are no remaining days or hours for the event, skip to the next iteration
+        continue;
+    }
+}
